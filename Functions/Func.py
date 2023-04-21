cd0c9d4d644a2c54f11bb6392a18f018fd423d7d
Shay Pasvolsky <spasvolski@gmail.com>
2023-04-22 01:35:19 +0300

    Added rename Feature

--- Functions/Func.py
+++ Functions/Func.py
@@ -15,5 +15,163 @@
 # Imports #
 ###########
 
-def TestFunc():
-    print("Test")
+import os           # Used to build the directory list, rename function
+import re           # For Regex Strings
+
+#######################
+# Function Defenition #
+#######################
+
+
+# Get all the movie and subtitle files in a path
+def Get_Files_In_Show_Folder(Show_Path, MFileType, SubFileType):
+    try:
+        Show_Files = []  # Declare blank show file array
+        # Get Files
+        Show_Files = [os.path.join(root, name) for root, dirs, files in os.walk(
+            Show_Path) for name in files if name.lower().endswith((MFileType, SubFileType))]
+        # Make sure files were found, if not reset the list
+        if (len(Show_Files) == 0):
+            Show_Files = ["Empty"]
+        # Return what was found
+        return Show_Files
+    except:
+        # return an error
+        return ["Error!!"]
+
+
+# Extract Season and Episode Numbers
+def Get_Season_Episode(File_Path):
+    try:
+        # Get Season Number Without The S
+        Seas = re.split(
+            r'S', (re.search(r'S\d+', File_Path.upper()).group()), maxsplit=2)[1]
+        # Get Episode Number With The E's
+        try:
+            # Check for Ex-Ex format for multi-episode
+            Epi = re.search(r'E\d+-E\d+', File_Path.upper()).group()
+        except:
+            try:
+                # Check for ExEx format for multi-episode
+                Epi = re.search(r'E\d+E\d+', File_Path.upper()).group()
+            except:
+                # Check for Ex format for single episode
+                Epi = re.search(r'E\d+', File_Path.upper()).group()
+        # Return Season and Episode Number
+        return [Seas, Epi]
+    except:
+        # Return Unvalid Values
+        return ["Empty", "Empty"]
+
+
+# Extract TV Show Or Movie Name
+def Get_TV_Movie_Name(File_Path, FType, MPfx, SPfx):
+    try:
+        # Try To Get The TV Show Or Movie Name
+        if (FType == "TV"):
+            TVMovie_Name = re.split(
+                r' -', (re.split(r'.S\d+E\d+', (re.split(r'/', File_Path)[-1]))[0]))[0]
+        elif (FType == "Movie"):
+            # Build The Delimiter To Check
+            Deli = MPfx.upper() + "|" + MPfx.lower() + "|" + \
+                SPfx.upper() + "|" + SPfx.lower()
+            TVMovie_Name = re.split(Deli, re.split(r'/', File_Path)[-1])[0]
+        else:
+            TVMovie_Name = "No_Name"
+        return TVMovie_Name
+    except:
+        # Return Error
+        return "Not A TV Show Or Movie File!"
+# os.path.filen
+
+
+# Build New Path To Arrange The File
+def Build_New_Name(File_Path, Season, Episode, ToM, NName, SeasPFX, SLang, MPfx, SPfx):
+    try:
+        New_Path = os.path.dirname(File_Path) + "/"
+        # Check if TV Or Movie Title
+        if (ToM == "TV"):
+            # Get Start Episode And End Episode
+            Temp_Epi = re.split(r'E|-E', Episode.upper(), maxsplit=2)
+            # Build The Episode Name
+            New_Name = NName + " - S" + Season + "E" + Temp_Epi[1]
+            # If the original file is a multi episode, adapt the new episode name
+            if (len(Temp_Epi) > 2):
+                New_Name += "-E" + Temp_Epi[2]
+        elif (ToM == "Movie"):
+            # Change The Movie Name To The New Name
+            New_Name = NName
+        else:
+            # Error Out
+            New_Name = "Error!!"
+        # Check if MKV Or SRT file
+        if (len(re.split(MPfx.upper(), File_Path.upper())) > 1):
+            File_Type = "Mkv"
+        else:
+            File_Type = "Sub"
+        if (New_Name != "Error!!"):
+            if (File_Type == "Sub"):
+                New_Name = New_Name + SLang + SPfx.lower()
+            else:
+                New_Name += MPfx.lower()
+        New_Path += New_Name
+        return New_Name, New_Path
+    except:
+        # Error Out
+        return ["Error!!!", "Error!!!"]
+
+
+# Validate New TV Or Movie Name
+def Val_New_Name(New_Name, Old_Name):
+    try:
+        # Check if user supplied a new name, if yes, return it.
+        if (New_Name != None):
+            return New_Name
+        else:  # Else return the extracted name
+            return Old_Name
+    except:
+        # Return Failed If Failed To Run
+        return Old_Name
+
+
+# Move To New Location
+def Rename_TV_Movie(Org_File, New_Name, MPfx):
+    try:
+        # if (Val_One_TV_Movie(Org_File, MPfx))
+        # Build The New File Path
+        New_Path = os.path.dirname(Org_File) + "/" + New_Name
+        # Rename The File
+        os.rename(Org_File, New_Path)
+        return True
+    except:
+        return False
+
+
+# Check If Path Has More Then One TV Show And / Or Movie
+def Val_One_TV_Movie(Files_In_Dir, MPfx, SPfx):
+    try:
+        Count = 0  # Set blank counter
+        # Detect If Movie
+        if (Get_Season_Episode(Files_In_Dir[0])[0] == "Empty"):
+            MoT = "Movie"
+        else:  # Set To TV
+            MoT = "TV"
+        # Get The First TV Show Or Movie Name
+        FName = Get_TV_Movie_Name(Files_In_Dir[0], MoT, MPfx, SPfx)
+        for f in Files_In_Dir:
+            # Detect If Movie
+            if (Get_Season_Episode(f)[0] == "Empty"):
+                MoT = "Movie"
+            else:  # Set To TV
+                MoT = "TV"
+            # Get The First TV Show Or Movie Name
+            F_Name = Get_TV_Movie_Name(f, MoT, MPfx, SPfx)
+            # Check If Not Identical To First Name And Increment The Counter
+            if (F_Name != FName):
+                Count += 1
+        if (Count):  # If More Then One Name Return False, Else Return True
+            return False
+        else:
+            return True
+    except:
+        return False
--- MET.py
+++ MET.py
@@ -28,11 +28,12 @@ parser = argparse.ArgumentParser(description="**********************************
                                  usage='%(prog)s [OPTIONS]', formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('-S', '-s', '--Source', required=True,
                     help="Source directory of the TV Show or Movie. Required to operate!")
-#parser.add_argument('-N', '-n', '--NewSName', help="New Name for the show")
+parser.add_argument('-N', '-n', '--NewSName',
+                    help="New Name for the TV Show or Movie. Does not run if more then one TV Show or Movie if the path!")
 # parser.add_argument('-O', '-o', '--Organaize', action='store_true',
 #                    help="Organize the show - add a sub dir for Season (with number) and move the files inside, then rename based on the formating. make sure there are no subfolders inside!!")
-# parser.add_argument('-R', '-r', '--ReName', action='store_true',
-#                    help="Rename the main show folder.")
+parser.add_argument('-R', '-r', '--ReName', action='store_true',
+                    help="Rename the TV show or Movie files.")
 # parser.add_argument('-S', '-s', '--SeasonRename', action='store_true',
 #                    help="Rename the season folder\s.")
 # parser.add_argument('-M', '-m', '--Move',
@@ -45,7 +46,22 @@ args = parser.parse_args()
 # Global Variables Defenition #
 ###############################
 
-src = ""  # Place Holder For Source Folder
+Ver = "0.1.0-0"     # Release number
+src = ""            # Place Holder For Source Folder
+dst = ""            # Place Holder For Destination Folder
+NewName = ""        # Place Holder For New TV Show Or Movie
+
+###########################
+# Do Not Change From Here #
+###########################
+
+# Show_Season_Dirs = []               # Seasons List
+Files_In_Dir = []                   # File List
+File_Sufix_Movies = ".mkv"          # Movie File Suffix
+File_Sufix_Subtitles = ".srt"       # Movie File Suffix
+Lang_File_Sufix = ".heb"            # Language Suffix For The Subtitles
+Season_Folder_Prefix = "Season"     # Season folder prefix before the season number
+
 
 ###################
 # Start the Logic #
@@ -55,9 +71,47 @@ src = ""  # Place Holder For Source Folder
 # Main function to run
 def main():
     try:
-        TestFunc()
+        # Make Sure the path is valid and get the files inside
+        if (os.path.isdir(args.Source)):
+            # Set Source Folder
+            src = args.Source
+            # Set Destination Folder
+            dst = src
+            # Get the file list
+            Files_In_Dir = Get_Files_In_Show_Folder(
+                src, File_Sufix_Movies, File_Sufix_Subtitles)
+            # Check If Path Contains Only On TV Show Or One Movie
+            OneSM = Val_One_TV_Movie(
+                Files_In_Dir, File_Sufix_Movies, File_Sufix_Subtitles)
+            # Run threw each file
+            for f in Files_In_Dir:
+                # Get Season and Episde(s) for the file
+                Seas, Epi = Get_Season_Episode(f)
+                # Check If There Is A Season And Decide If It Is A Movie Or TV
+                if (Seas == "Empty"):  # Movie
+                    TVMName = Get_TV_Movie_Name(
+                        f, "Movie", File_Sufix_Movies, File_Sufix_Subtitles)  # Get TV Or Movie Name
+                    # If user supplied new name for the show or movie, Use it.
+                    NewName = Val_New_Name(args.NewSName, TVMName)
+                    # Get The Correct New Name And Path
+                    ANew_Name, New_Path = Build_New_Name(f, "", "", "Movie", NewName,
+                                                         "", Lang_File_Sufix, File_Sufix_Movies, File_Sufix_Subtitles)
+                else:  # TV Show
+                    TVMName = Get_TV_Movie_Name(
+                        f, "TV", File_Sufix_Movies, File_Sufix_Subtitles)  # Get TV Or Movie Name
+                    # If user supplied new name for the show or movie, Use it.
+                    NewName = Val_New_Name(args.NewSName, TVMName)
+                    # Get The Correct New Name And Path
+                    ANew_Name, New_Path = Build_New_Name(f, Seas, Epi, "TV", NewName,
+                                                         Season_Folder_Prefix, Lang_File_Sufix, File_Sufix_Movies, File_Sufix_Subtitles)
+                if (args.ReName and OneSM):  # Rename If User Asked To
+                    Rename_TV_Movie(f, ANew_Name, File_Sufix_Movies)
+                #print(NewName, " -> ", ANew_Name, " -> ", New_Path)
+        else:
+            # Error on path not valid
+            print("Not A Valid Folder, quiting!!!")
     except:
-        print("Error!!!!")
+        print("UnKnown Error!!!!")
 
 
 # Make sure to run only if called directly
